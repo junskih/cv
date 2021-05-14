2ea87462d07a1da0ac7d2e8d1bb7ef9dc9e52e8f
junskih <46686957+junskih@users.noreply.github.com>
2021-05-14 20:41:51 +0300

    Add basis for information structure and its display

--- src/App.js
+++ src/App.js
@@ -1,9 +1,71 @@
+import React from 'react';
 import './App.css';
+import uniqid from 'uniqid';
+import Form from './components/Form';
+import CV from './components/CV';
 
-function App() {
-  return (
-    <p>Yes</p>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.formSections = [
+      {
+        id: uniqid(),
+        title: 'personal information',
+        fields: [
+          'first name',
+          'last name',
+          'title',
+          'company/school',
+          'city',
+          'zipcode',
+          'address',
+          'phone',
+          'email',
+          'description'
+        ],
+        deletable: false
+      },
+
+      {
+        id: uniqid(),
+        title: 'education',
+        fields: [
+          'degree',
+          'institution',
+          'from',
+          'to'
+        ],
+        deletable: true
+      },
+
+      {
+        id: uniqid(),
+        title: 'experience',
+        fields: [
+          'title',
+          'employer',
+          'from',
+          'to'
+        ],
+        deletable: true
+      }
+    ];
+  }
+  
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>CV</h1>
+        </header>
+        <main>
+          <Form formSections={this.formSections} />
+          <CV formSections={this.formSections} />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/CV.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class CV extends React.Component {
+  render() {
+    return (
+      <div className='cv'>
+        <div className='cv-header'>
+          CV
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CV;
