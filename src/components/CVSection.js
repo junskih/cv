745b520d1d3d95d3d02be916aaaa2ce52a929e0f
junskih <46686957+junskih@users.noreply.github.com>
2021-05-28 21:23:39 +0300

    Update to use generic children prop

--- src/components/CVSection.js
+++ src/components/CVSection.js
@@ -1,28 +1,16 @@
 import React from 'react';
-import CVEntry from './CVEntry';
-import CVField from './CVField'
 import { capitalize } from '../utils';
 
 class CVSection extends React.Component {
   render() {
     const {
-      sectionTitle,
-      sectionData
+      sectionTitle
     } = this.props;
-
-    let sectionInfo;
-    if (Array.isArray(sectionData)) {
-      sectionInfo = sectionData.map(entry => {
-        return <CVEntry key={entry.id} entryData={entry.fields}/>;
-      });
-    } else if (typeof sectionData === 'string') {
-      sectionInfo = <CVField key={sectionData} value={sectionData} />;
-    }
 
     return (
       <div className='cv-section'>
         <h3 className='cv-section-title'>{capitalize(sectionTitle)}</h3>
-        {sectionInfo}
+        {this.props.children}
       </div>
     );
   }
