5d57abd74618f996fa0e34c26220cb4f39e888a5
junskih <46686957+junskih@users.noreply.github.com>
2021-05-28 21:25:04 +0300

    Update to use nested and more specialized components

--- src/components/CV.js
+++ src/components/CV.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import CVMainEntry from './CVMainEntry';
+import CVSection from './CVSection';
+import CVSidebarEntry from './CVSidebarEntry';
 
 class CV extends React.Component {
   render() {
+    const {
+      state
+    } = this.props;
+
+    const personal = state['personal information'][0].fields;
+    const education = state['education'].map(entry => {
+      return <CVMainEntry key={entry.id} entryData={entry.fields} />
+    });
+    const experience = state['experience'].map(entry => {
+      return <CVMainEntry key={entry.id} entryData={entry.fields} />
+    });
+    
     return (
       <div className='cv'>
         <div className='cv-header'>
-          CV
+          <h2>{`${personal['first name']} ${personal['last name']}`}</h2>
+          <h4>{personal['title']}</h4>
+        </div>
+        <div className='cv-sidebar'>
+          <CVSection sectionTitle={'personal details'} >
+            <CVSidebarEntry entryTitle={'address'} entryData={personal['address']} />
+            <CVSidebarEntry entryTitle={'email'} entryData={personal['email']} />
+            <CVSidebarEntry entryTitle={'phone number'} entryData={personal['phone number']} />
+          </CVSection>
+        </div>
+        <div className='cv-main'>
+          <CVSection sectionTitle={'description'}>
+            <p>{personal['description']}</p>
+          </CVSection>
+          <CVSection sectionTitle={'education'}>
+            {education}
+          </CVSection>
+          <CVSection sectionTitle={'experience'}>
+            {experience}
+          </CVSection>
         </div>
       </div>
     );
